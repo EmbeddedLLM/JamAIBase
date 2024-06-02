0e6b8e6dc017fe62f8ef88858fbb13530f7385e7
Jia Huei Tan <tanjiahuei@gmail.com>
2024-06-02 16:19:48 +0000

    CI bug fix

--- scripts/remove_cloud_modules.sh
+++ scripts/remove_cloud_modules.sh
@@ -3,5 +3,5 @@
 find . -type f -name "cloud*.py" -delete
 find . -type f -name "compose.*.cloud.yml" -delete
 find . -type d -name "(cloud)" -exec rm -rf {} +
-rm services/app/ecosystem.config.cjs
-rm services/app/ecosystem.json
+rm -f services/app/ecosystem.config.cjs
+rm -f services/app/ecosystem.json
