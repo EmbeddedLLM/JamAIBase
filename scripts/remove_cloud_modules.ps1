45cbc22053068672447d090060a4c024da4b6947
Jia Huei Tan <tanjiahuei@gmail.com>
2024-07-23 08:39:56 +0000

    Fix OSS CI-Win: Ignore non-existent file

--- scripts/remove_cloud_modules.ps1
+++ scripts/remove_cloud_modules.ps1
@@ -2,5 +2,15 @@ Get-ChildItem -Recurse -File -Filter "cloud*.py" | Remove-Item -Force
 Get-ChildItem -Recurse -File -Filter "cloud*.json" | Remove-Item -Force
 Get-ChildItem -Recurse -File -Filter "compose.*.cloud.yml" | Remove-Item -Force
 Get-ChildItem -Recurse -Directory -Filter "(cloud)" | Remove-Item -Recurse -Force
-Remove-Item -Force "services/app/ecosystem.config.cjs"
-Remove-Item -Force "services/app/ecosystem.json"
+
+# Remove a file or folder quietly
+# Like linux "rm -rf"
+function quiet_rm($item)
+{
+  if (Test-Path $item) {
+    echo "Removing $item"
+    Remove-Item -Force $item
+  }
+}
+quiet_rm "services/app/ecosystem.config.cjs"
+quiet_rm "services/appecosystem.json"
