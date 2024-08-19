76fa054408cc4f77a9f3c17eaf1ec703d8c5955d
tjtanaa <tunjian.tan@embeddedllm.com>
2024-08-19 10:59:51 +0800

    fix docio ci

--- scripts/compile_docio_exe.ps1
+++ scripts/compile_docio_exe.ps1
@@ -3,5 +3,7 @@ cd .\clients\python
 pip install .
 cd .\..\..\services\docio
 pip install -e .
-pip install pyinstaller
+pip install pyinstaller==6.9.0
+pip install cryptography==42.0.8
+pip install python-magic-bin
 pyinstaller docio.spec
